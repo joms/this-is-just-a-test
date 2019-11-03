d23fd4e8739c645808368f31becfffbf3fb1b18f
Jo Emil Holen <jo-emil@outlook.com>
2019-11-03 22:54:05 +0100

    make design responsive on mobile

--- src/App.js
+++ src/App.js
@@ -185,12 +185,14 @@ function App() {
 
     return (
         <main>
-            <h1>Kjøp Bilforsikring</h1>
-            <p>
-                Det er fire forskjellige forsikringer å velge mellom. Ansvarsforsikring er lovpålagt om kjøretøyet er
-                registrert og skal brukes på veien. I tillegg kan du utvide forsikringen avhengig av hvor gammel bilen
-                din er og hvordan du bruker den
-            </p>
+            <header>
+                <h1>Kjøp Bilforsikring</h1>
+                <p>
+                    Det er fire forskjellige forsikringer å velge mellom. Ansvarsforsikring er lovpålagt om kjøretøyet
+                    er registrert og skal brukes på veien. I tillegg kan du utvide forsikringen avhengig av hvor gammel
+                    bilen din er og hvordan du bruker den
+                </p>
+            </header>
             {formResult && (
                 <div id="pricelist">
                     <img src="/lada.jpg" alt="Lada" />
